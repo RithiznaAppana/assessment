b0f347e2598f8d1f8a61211ee95cf43df002c07c
Rizzz <appanarithizna@gmail.com>
2026-02-19 10:09:27 +0530

    Using env variables in frontend

--- frontend/src/components/Quiz.js
+++ frontend/src/components/Quiz.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 const Quiz = () => {
   const { topicId } = useParams();
